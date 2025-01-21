469bbe50cfce759013fa5d46f2374bf9c2943cb0
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-21 16:24:40 +0200

    Add adaptive layout helper and platform constants for responsive design

---
+++ lib/core/helpers/constants/keys.dart
@@ -0,0 +1,8 @@
+class SharedPrefKeys {
+  static const String isOnBoarding = 'onBoarding';
+  static const String userToken = 'userToken';
+  static const String userData = 'userData';
+}
+
+bool isLoggedInUser = false;
+bool isOnBoardingView = false;
