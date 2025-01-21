e06f2aae5f8fa5997774f30df4513e7ec43b48c6
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-21 16:17:57 +0200

    Integrate ScreenUtil for responsive design and restrict orientation
    Added flutter_screenutil package to handle responsive design based on screen size. Also, restricted the app to portrait orientation only.

--- lib/fakahani_dashboard.dart
+++ lib/fakahani_dashboard.dart
@@ -1,6 +1,8 @@
 import 'package:fakahani_dashboard/core/routing/app_router.dart';
 import 'package:fakahani_dashboard/core/routing/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FakahaniDashboard extends StatelessWidget {
   const FakahaniDashboard({super.key, required this.appRouter});
@@ -8,11 +10,27 @@ class FakahaniDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Fakahani Dashboard',
-      onGenerateRoute: appRouter.generateRoute,
-      initialRoute: Routes.dashboardView,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ]);
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            textScaler: const TextScaler.linear(1.0),
+          ),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Fakahani Dashboard',
+            onGenerateRoute: appRouter.generateRoute,
+            initialRoute: Routes.dashboardView,
+          ),
+        );
+      },
     );
   }
 }
