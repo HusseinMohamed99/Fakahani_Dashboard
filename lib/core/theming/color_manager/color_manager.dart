6324a2147e0bb0aacc251347f7eff53436fdc752
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-21 16:26:56 +0200

    Add color manager, font family helper, and font size helper for theming

---
+++ lib/core/theming/color_manager/color_manager.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  /// Primary Color For App
+  static const Color primaryGreen = Color(0xFF1B5E37); // Formerly green
+  static const Color brightGreen = Color(0xFF5DB957); // Formerly lightGreen
+  static const Color lighterGreen = Color(0xff9ED8A9); // Very light green
+  static const Color mediumGreen = Color(0xFF2D9F5D); // Formerly darkGreen
+  static const Color pureBlack = Color(0xFF000000); // Formerly black
+  static const Color darkBlack = Color(0xFF0C0D0D); // Formerly secondaryBlack
+  static const Color pureWhite = Color(0xFFFFFFFF); // Formerly white
+  static const Color softWhite = Color(0xFFF9FAFA); // Formerly secondaryWhite
+  static const Color offWhite = Color(0xFFE6E9E9); // Soft, muted white
+  static const Color cardBackgroundColor = Color(0xFFF3F5F7);
+  static const Color vibrantOrange = Color(0xFFF4A91F); // Formerly orange
+  static const Color softOrange = Color(0xFFF8C76D); // Formerly lightOrange
+  static const Color softGray = Color(0xFFEEEEEE); // Very light orange
+  static const Color darkGrey = Color(0xFF4E5556); // Formerly grey
+  static const Color mediumGrey = Color(0xFF949D9E); // Formerly lightGrey
+  static const Color lightGrey = Color(0xFFC9CECF); // Formerly lighterGrey
+  static const Color lighterGrey = Color(0xFFDCDEDE); // Very light grey
+  static const Color brightRed = Color(0xFFF30202); // Formerly red
+  static const Color errorRed = Color(0xFFB00020); // Red color for error text
+  static const Color notificationShape = Color(0xFFEEF8ED);
+}
