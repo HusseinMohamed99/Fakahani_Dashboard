89019c7ca65b59207b1aa57e41c395940aba13f1
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-22 16:14:17 +0200

    Implement BlocObserver and dependency injection setup; update main initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:fakahani_dashboard/bloc_observer.dart';
+import 'package:fakahani_dashboard/core/di/dependency_injection.dart';
 import 'package:fakahani_dashboard/core/routing/app_router.dart';
 import 'package:fakahani_dashboard/fakahani_dashboard.dart';
+import 'package:fakahani_dashboard/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initServices();
+
   runApp(
     FakahaniDashboard(appRouter: AppRouters()),
   );
+}
+
+Future<void> initServices() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await setupGetIt();
+  await ScreenUtil.ensureScreenSize();
+  Bloc.observer = MyBlocObserver();
 }
