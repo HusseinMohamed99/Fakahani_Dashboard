c0211f6b0706e42fa6f7a982b8a40a1a4b461cfd
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-23 15:13:12 +0200

    Integrate Firebase Storage reference in FireStorage class for file upload functionality

--- lib/core/networking/services/fire_storage.dart
+++ lib/core/networking/services/fire_storage.dart
@@ -1,8 +1,10 @@
 import 'package:fakahani_dashboard/core/networking/services/storage_service.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class FireStorage implements StorageService {
   @override
   Future<String> uploadFile(String file) {
+    final FirebaseStorage storageReference = FirebaseStorage.instance;
     // TODO: implement uploadFile
     throw UnimplementedError();
   }
