034c5563e7e20e94630afcb8a55706d22df8f35e
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-23 14:33:21 +0200

    Add AddProductInputEntity class for product input handling

---
+++ lib/features/add_product/domain/entities/add_product_input_entity.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+class AddProductInputEntity {
+  AddProductInputEntity(
+      {required this.name,
+      required this.code,
+      required this.description,
+      required this.price,
+      required this.isFeaturedItem,
+      required this.image,
+      this.imageURL});
+
+  final String name;
+  final String code;
+  final String description;
+  final num price;
+  final bool isFeaturedItem;
+  final File image;
+  final String? imageURL;
+}
