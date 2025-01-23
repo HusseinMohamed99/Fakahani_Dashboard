a55c291769eddc041f1e6b26644e93c62af29deb
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-23 11:36:30 +0200

    Add product form fields and padding
    Added `CustomTextFormField` widgets for product name, price, code, and description. Also, added padding to the form using `allPadding` extension.

--- lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
+++ lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
@@ -1,4 +1,8 @@
+import 'package:fakahani_dashboard/core/helpers/extensions/widget_extension.dart';
+import 'package:fakahani_dashboard/core/helpers/value_manager/dimensions.dart';
+import 'package:fakahani_dashboard/core/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AddProductViewBody extends StatefulWidget {
   const AddProductViewBody({super.key});
@@ -10,6 +14,11 @@ class AddProductViewBody extends StatefulWidget {
 class _AddProductViewBodyState extends State<AddProductViewBody> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  final TextEditingController productNameController = TextEditingController();
+  final TextEditingController productDescriptionController =
+      TextEditingController();
+  final TextEditingController productPriceController = TextEditingController();
+  final TextEditingController productCodeController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
@@ -18,12 +27,36 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
           child: Form(
             key: _formKey,
             autovalidateMode: _autovalidateMode,
-            child: const Column(
-              children: [],
+            child: Column(
+              spacing: kSpacingDefault.h,
+              children: [
+                CustomTextFormField(
+                  hintText: 'Product Name',
+                  controller: productNameController,
+                ),
+                CustomTextFormField(
+                  hintText: 'Product Price',
+                  controller: productPriceController,
+                  textInputType: TextInputType.number,
+                ),
+                CustomTextFormField(
+                  hintText: 'Product code',
+                  controller: productCodeController,
+                  textInputType: TextInputType.number,
+                ),
+                CustomTextFormField(
+                  hintText: 'Product Description',
+                  controller: productDescriptionController,
+                  maxLines: 5,
+                ),
+              ],
             ),
           ),
         ),
       ],
+    ).allPadding(
+      vPadding: kPaddingDefaultVertical,
+      hPadding: kPaddingDefaultHorizontal,
     );
   }
 }
