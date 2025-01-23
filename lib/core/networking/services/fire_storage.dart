55318b48da4b7382ed42e3513dd6907fa3f686ac
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-23 15:11:27 +0200

    Add FireStorage implementation and abstract StorageService class for file upload functionality

---
+++ lib/core/networking/services/fire_storage.dart
@@ -0,0 +1,9 @@
+import 'package:fakahani_dashboard/core/networking/services/storage_service.dart';
+
+class FireStorage implements StorageService {
+  @override
+  Future<String> uploadFile(String file) {
+    // TODO: implement uploadFile
+    throw UnimplementedError();
+  }
+}
