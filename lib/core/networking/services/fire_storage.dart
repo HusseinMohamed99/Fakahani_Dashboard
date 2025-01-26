222cef30cabc876613e3f2957361d23f3d1afbb8
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-26 15:42:58 +0200

    Implement file upload functionality in FireStorage
    Updated the `FireStorage` class to implement the `uploadFile` method, which now accepts a `File` object and a `String` path. The method constructs a file reference using the provided path and file name, uploads the file to Firebase Storage, and returns the download URL.
    
    Also, modified the `StorageService` abstract class to update the `uploadFile` method signature to accept a `File` object and a `String` path.
    
    Added the `path` package as a direct dependency in `pubspec.yaml` and updated the `pubspec.lock` file accordingly.

--- lib/core/networking/services/fire_storage.dart
+++ lib/core/networking/services/fire_storage.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:fakahani_dashboard/core/networking/services/storage_service.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart' as b;
 
 class FireStorage implements StorageService {
+  final firebaseStorage = FirebaseStorage.instance.ref();
   @override
-  Future<String> uploadFile(String file) {
-    final FirebaseStorage storageReference = FirebaseStorage.instance;
-    // TODO: implement uploadFile
-    throw UnimplementedError();
+  Future<String> uploadFile(File file, String path) async {
+    String fileName = b.basename(file.path);
+    String extensionName = b.extension(file.path);
+    var fileReference = firebaseStorage.child('$path/$fileName.$extensionName');
+    await fileReference.putFile(file);
+    var fileURL = fileReference.getDownloadURL();
+    return fileURL;
   }
 }
