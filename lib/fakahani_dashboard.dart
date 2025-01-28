49db057d00d3fd5ea67ae90046eb034e74d3c3e9
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-28 16:22:39 +0200

    Add Arabic localization support to Fakahani Dashboard
    This commit adds support for Arabic localization in the Fakahani Dashboard application. It includes the necessary imports for localization and configures the `MaterialApp` to use Arabic as the default locale, along with the appropriate delegates and supported locales.

--- lib/fakahani_dashboard.dart
+++ lib/fakahani_dashboard.dart
@@ -2,8 +2,10 @@ import 'package:fakahani_dashboard/core/helpers/constants/keys.dart';
 import 'package:fakahani_dashboard/core/routing/app_router.dart';
 import 'package:fakahani_dashboard/core/routing/routes.dart';
 import 'package:fakahani_dashboard/core/theming/app_theming/light_theming.dart';
+import 'package:fakahani_dashboard/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FakahaniDashboard extends StatelessWidget {
@@ -26,7 +28,15 @@ class FakahaniDashboard extends StatelessWidget {
             textScaler: const TextScaler.linear(1.0),
           ),
           child: MaterialApp(
+            locale: const Locale('ar'),
             debugShowCheckedModeBanner: false,
+            localizationsDelegates: [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
             title: 'Fakahani Dashboard',
             theme: buildLightTheming(),
             themeMode: ThemeMode.light,
