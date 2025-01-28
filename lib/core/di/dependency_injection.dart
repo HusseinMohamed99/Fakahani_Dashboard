e3bdb849f2f3f50fefeb1da34d2a55dc593bfa8d
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-28 15:48:28 +0200

    Add FirebaseAuthServices and AuthRepo to dependency injection
    Added FirebaseAuthServices and AuthRepo to the dependency injection setup. Also, included the initialization of CachingHelper and corrected the AuthRepoImpl registration.

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,4 +1,6 @@
+import 'package:fakahani_dashboard/core/networking/cache/caching_helper.dart';
 import 'package:fakahani_dashboard/core/networking/services/database_service.dart';
+import 'package:fakahani_dashboard/core/networking/services/firebase_auth_services.dart';
 import 'package:fakahani_dashboard/core/networking/services/firestore_services.dart';
 import 'package:fakahani_dashboard/core/networking/services/storage_service.dart';
 import 'package:fakahani_dashboard/core/networking/services/supabase_storage.dart';
@@ -6,11 +8,21 @@ import 'package:fakahani_dashboard/core/repos/images_repo/images_repo.dart';
 import 'package:fakahani_dashboard/core/repos/images_repo/images_repo_impl.dart';
 import 'package:fakahani_dashboard/core/repos/product_repo/product_repo.dart';
 import 'package:fakahani_dashboard/core/repos/product_repo/product_repo_impl.dart';
+import 'package:fakahani_dashboard/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:fakahani_dashboard/features/auth/domain/repo/auth_repo.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> setupGetIt() async {
+  await CachingHelper.init();
+  getIt.registerSingleton<FirebaseAuthServices>(FirebaseAuthServices());
+  getIt.registerSingleton<AuthRepo>(
+    AuthRepoImpl(
+      firebaseAuthServices: getIt<FirebaseAuthServices>(),
+      databaseServices: getIt<DatabaseServices>(),
+    ),
+  );
   getIt.registerSingleton<StorageService>(SupabaseStorageService());
   getIt.registerSingleton<DatabaseServices>(FirestoreServices());
   getIt.registerSingleton<ImagesRepo>(
