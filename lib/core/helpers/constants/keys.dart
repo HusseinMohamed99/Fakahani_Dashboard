44caefd71bafa699de3595113204a07ff2d2cc0f
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-28 13:47:51 +0200

    Add Supabase integration and update plugin dependencies
    Integrate Supabase for backend services and update the project dependencies to include necessary plugins for Linux, macOS, and Windows platforms. This includes adding Supabase Flutter, URL Launcher, and other related plugins to support cross-platform functionality.

--- lib/core/helpers/constants/keys.dart
+++ lib/core/helpers/constants/keys.dart
@@ -6,3 +6,7 @@ class SharedPrefKeys {
 
 bool isLoggedInUser = false;
 bool isOnBoardingView = false;
+
+const String kSupabaseApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6Z3dxcW1pcm9ydW5laHB4bXBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwNTg1MzksImV4cCI6MjA1MzYzNDUzOX0.AoxNCHOaUaoEnRvgLt3ppXB7wIvltS2a3quOASS70r8';
+const String kSupabaseUrl = 'https://wzgwqqmirorunehpxmps.supabase.co';
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:fakahani_dashboard/bloc_observer.dart';
 import 'package:fakahani_dashboard/core/di/dependency_injection.dart';
+import 'package:fakahani_dashboard/core/helpers/constants/keys.dart';
 import 'package:fakahani_dashboard/core/routing/app_router.dart';
 import 'package:fakahani_dashboard/fakahani_dashboard.dart';
 import 'package:fakahani_dashboard/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +22,10 @@ void main() async {
 Future<void> initServices() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Supabase.initialize(
+    url: kSupabaseUrl,
+    anonKey: kSupabaseApiKey,
   );
   await setupGetIt();
   await ScreenUtil.ensureScreenSize();
