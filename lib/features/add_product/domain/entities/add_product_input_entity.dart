12ec8bed056794065b217649da49802ff90a71a5
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-28 11:04:55 +0200

    Add expiryMonth, numberOfCalories, and uintAmount to AddProductInputEntity
    Updated the AddProductInputEntity class to include additional fields: expiryMonth, numberOfCalories, and uintAmount. Also added default values for isOrganic, avgRating, and ratingCount.

--- lib/features/add_product/domain/entities/add_product_input_entity.dart
+++ lib/features/add_product/domain/entities/add_product_input_entity.dart
@@ -1,14 +1,18 @@
 import 'dart:io';
 
 class AddProductInputEntity {
-  AddProductInputEntity(
-      {required this.name,
-      required this.code,
-      required this.description,
-      required this.price,
-      required this.isFeaturedItem,
-      required this.image,
-      this.imageURL});
+  AddProductInputEntity({
+    required this.name,
+    required this.code,
+    required this.description,
+    required this.price,
+    required this.isFeaturedItem,
+    required this.image,
+    this.imageURL,
+    required this.expiryMonth,
+    required this.numberOfCalories,
+    required this.uintAmount,
+  });
 
   final String name;
   final String code;
@@ -17,4 +21,10 @@ class AddProductInputEntity {
   final bool isFeaturedItem;
   final File image;
   String? imageURL;
+  final int expiryMonth;
+  final int numberOfCalories;
+  final int uintAmount;
+  final bool isOrganic = false;
+  final num avgRating = 0;
+  final int ratingCount = 0;
 }
