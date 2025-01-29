0c97df694c622972bdbd4f20af408af928bba7e1
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-29 12:48:44 +0200

    Add welcome text and update button label in DashboardViewBoy
    Added a `WelcomeText` widget to display a welcoming message with styled text. Updated the button label from "Add Product" to "Get Started" for better user guidance.

--- lib/features/dashboard/presentation/views/widgets/dashboard_view_boy.dart
+++ lib/features/dashboard/presentation/views/widgets/dashboard_view_boy.dart
@@ -3,6 +3,8 @@ import 'package:fakahani_dashboard/core/helpers/extensions/responsive_size_exten
 import 'package:fakahani_dashboard/core/helpers/extensions/widget_extension.dart';
 import 'package:fakahani_dashboard/core/helpers/value_manager/dimensions.dart';
 import 'package:fakahani_dashboard/core/routing/routes.dart';
+import 'package:fakahani_dashboard/core/theming/color_manager/color_manager.dart';
+import 'package:fakahani_dashboard/core/theming/style_manager/text_style.dart';
 import 'package:fakahani_dashboard/core/widgets/custom_button.dart';
 import 'package:fakahani_dashboard/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
@@ -38,9 +40,10 @@ class DashboardViewBoy extends StatelessWidget {
             ],
           ),
         ),
+        const WelcomeText(),
         const Spacer(),
         CustomButton(
-          text: 'Add Product',
+          text: 'Get Started',
           onPressed: () {
             context.pushNamed(Routes.addProductView);
           },
@@ -49,6 +52,61 @@ class DashboardViewBoy extends StatelessWidget {
           hPadding: kPaddingDefaultVertical.w,
         ),
       ],
+    );
+  }
+}
+
+class WelcomeText extends StatelessWidget {
+  const WelcomeText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: kSpacingXLarge.h,
+      children: [
+        RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: 'Welcome ',
+                style: TextStyleManager.bold19(context: context),
+              ),
+              TextSpan(
+                text: 'to ',
+                style: TextStyleManager.bold19(context: context).copyWith(
+                  color: ColorManager.vibrantOrange,
+                ),
+              ),
+              TextSpan(
+                text: 'Faka',
+                style: TextStyleManager.bold19(context: context).copyWith(
+                  color: ColorManager.primaryGreen,
+                ),
+              ),
+              TextSpan(
+                text: 'hani',
+                style: TextStyleManager.bold19(context: context).copyWith(
+                  color: ColorManager.vibrantOrange,
+                ),
+              ),
+            ],
+            style: TextStyleManager.bold23(context: context),
+          ),
+        ),
+        Text(
+          '''We’re excited to see what you’re adding! Fill in the details below, and don’t forget
+           to upload some great images.
+        Your product will be live and ready for customers in just a few steps.''',
+          style: TextStyleManager.semiBold13(context: context),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    ).allPadding(
+      vPadding: kPaddingLargeHorizontal.h,
+      hPadding: kPaddingDefaultVertical.w,
     );
   }
 }
