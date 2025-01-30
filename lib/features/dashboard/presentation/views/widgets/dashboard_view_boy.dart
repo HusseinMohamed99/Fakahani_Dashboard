c42cb723edb4595ab59c00645301530757f106d4
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-30 11:05:05 +0200

    Add settings button to dashboard with logging functionality

--- lib/features/dashboard/presentation/views/widgets/dashboard_view_boy.dart
+++ lib/features/dashboard/presentation/views/widgets/dashboard_view_boy.dart
@@ -2,8 +2,10 @@ import 'package:fakahani_dashboard/core/helpers/extensions/localization_extensio
 import 'package:fakahani_dashboard/core/helpers/extensions/navigation_extension.dart';
 import 'package:fakahani_dashboard/core/helpers/extensions/responsive_size_extension.dart';
 import 'package:fakahani_dashboard/core/helpers/extensions/widget_extension.dart';
+import 'package:fakahani_dashboard/core/helpers/functions/app_logs.dart';
 import 'package:fakahani_dashboard/core/helpers/value_manager/dimensions.dart';
 import 'package:fakahani_dashboard/core/routing/routes.dart';
+import 'package:fakahani_dashboard/core/theming/color_manager/color_manager.dart';
 import 'package:fakahani_dashboard/core/widgets/custom_button.dart';
 import 'package:fakahani_dashboard/features/dashboard/presentation/views/widgets/welcome_text.dart';
 import 'package:fakahani_dashboard/gen/assets.gen.dart';
@@ -35,6 +37,24 @@ class DashboardViewBoy extends StatelessWidget {
                 right: 29.w,
                 child: SvgPicture.asset(
                   Assets.images.image,
+                ),
+              ),
+              Positioned(
+                top: context.screenHeight * 0.06,
+                right: context.screenWidth * 0.04,
+                child: CircleAvatar(
+                  radius: 20.r,
+                  backgroundColor: ColorManager.lighterGreen,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.settings_outlined,
+                      size: 24.sp,
+                    ),
+                    onPressed: () {
+                      //  context.pushNamed(Routes.profileView);
+                      AppLogs.infoLog('Settings');
+                    },
+                  ),
                 ),
               ),
             ],
